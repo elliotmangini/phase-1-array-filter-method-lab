b9d67de7de8138de08e6494cd2e43cc4d0f6201e
Elliot Mangini <elliot.mangini@gmail.com>
2022-08-11 17:35:46 -0600

    commit tree 2b692ff7b9194a591bc08bf6e3f097e5ced44090

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+
+
+const findMatching = (driverArray, string) => {
+    return driverArray.filter(driver => driver.toLowerCase() === string.toLowerCase());
+}
+
+const fuzzyMatch = (driverArray, string) => {
+    return driverArray.filter((driver) => driver[0] === string[0]);
+}
+
+const matchName = (driverArray, string) =>  {
+    return driverArray.filter((driver) => driver.name ===string);
+};
+
+
+
+// // This is roughly what I reduced it from.
+
+
+// const findMatching = (driverArray, string) => {
+//     return driverArray.filter(function (driver) {
+//         return driver.toLowerCase() === string.toLowerCase();
+//     })
+// }
+
+// const fuzzyMatch = (driverArray, string) => {
+//     return driverArray.filter(function (driver) {
+//         return driver[0] === string[0];
+//     })
+// }
+
+// const matchName = (driverArray, string) => {
+//     return driverArray.filter(function (driver) {
+//         return driver.toLowerCase() === string.toLowerCase();
+//     })
+// }
